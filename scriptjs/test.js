ac58e32336943cfa2e342aece2b931f8df77868e
Maksim <m.e.smirnov@yandex.ru>
2023-08-17 18:51:15 +0300

    practical 3

---
+++ scriptjs/test.js
@@ -0,0 +1,48 @@
+'use strict';
+
+// let txt = "КРОКОДИЛ";
+// console.log(txt.length);
+//length считает симфолы
+
+// let txt = "Всем привет, меня зовут Андрей Андрей Андрей";
+// console.log(txt.replace(/Андрей/g,"Вадиим"));
+
+// let txt = "Банан, Яблоко, Киви"
+// console.log(txt.toUpperCase());
+// let txt = "Банан, Яблоко, Киви"
+// console.log(txt.toLowerCase()); 
+//нижний и верхний регистр
+
+// let txt1 = "Меня зовут";
+// let txt2 = "Андрей";
+// console.log(txt1.concat('',txt2)); 
+// склеивание строк
+
+// let txt = "Банан, Яблоко, Киви";
+// console.log(txt[0]);
+
+
+// let txt = "а,б,в,г";
+// console.log(txt.split(","));
+// разделение 
+
+// let num = 159;
+// console.log(num.toString());
+
+// let num = 1.23456;
+// console.log(num.toExponential());
+
+// let num  = 1.23456;
+// console.log(num.toFixed(2)); 
+
+// let num = 1.23456;
+// console.log(num.valueOf());
+
+// console.log(parseFloat('10'));
+// console.log(parseFloat('10'));
+
+// let max = Number.MAX_VALUE;
+// console.log(max);
+// let min = Number.MIN_VALUE;
+// console.log(min);
+//максимальное и минимальное значкение в жава скрипт
